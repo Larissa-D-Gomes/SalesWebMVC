fe2e2436525f074a74106cd384009a8a1168bc72
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2021-01-26 14:34:39 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+
 
 namespace SalesWebMVC.Models
 {
@@ -9,5 +9,37 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //Constructors 
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        // Methods
+
+        /* Adds a new Seller 
+         * @param SalesRecord sr
+         */
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+
+        /* Returns the total sales amount for a given period of time
+         * @param DateTime initial, DateTime final
+         * @return double total
+         */
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return this.Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        //Constructor
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.BaseSalary = baseSalary;
+            this.BirthDate = birthDate;
+            this.Department = department;
+        }
+
+        // Methods
+
+        /* Adds a new Sale Record 
+         * @param SalesRecord sr
+         */
+        public void AddSale(SalesRecord sr)
+        {
+            this.Sales.Add(sr);
+        }
+
+        /* Removes a new Sale Record 
+         * @param SalesRecord sr
+         */
+        public void AddRemove(SalesRecord sr)
+        {
+            this.Sales.Remove(sr);
+        }
+
+        /* Returns the total sales amount for a given period of time
+         * @param DateTime initial, DateTime final
+         * @return double total
+         */
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return this.Sales.Where(p => initial <= p.Date && p.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
